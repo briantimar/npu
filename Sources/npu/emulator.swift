d5268f4411cf841db1c061f7b93337503c48eba7
Brian Timar <timarbrian@gmail.com>
2020-04-21 17:05:32 -0700

    test

--- Sources/npu/emulator.swift
+++ Sources/npu/emulator.swift
@@ -167,7 +167,6 @@ class VectorFeed : Cell {
     }
     
     func emit() {
-        
         outputBuffers![0].set(to: currentValue())
         advance()
     }
--- Tests/npuTests/Hardware.swift
+++ Tests/npuTests/Hardware.swift
@@ -76,6 +76,10 @@ class Hardware: XCTestCase {
             XCTAssertEqual(feed.outputBuffers![0].get(), [vals[i]])
         }
         XCTAssertTrue(feed.finished)
+        feed.emit()
+        XCTAssertEqual(feed.outputBuffers![0].get(), nil)
+        
+        
         feed.loadFrom(array: [5.0])
         XCTAssertFalse(feed.finished)
         
